ce142d4b013571226ef62a85673e1df1f9b771ea
jbisono26 <jeanbisono@outlook.com>
2020-12-21 11:54:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('hello!')
+
+const foodImages = [{
+  name:'fries',
+  img: 'images/fries.png'
+},
+{
+  name: 'cheeseburger',
+  img: 'images/cheeseburger.png'
+},
+{
+  name:'hotdog',
+  img: 'images/hotdog.png'
+},
+{
+  name:'ice cream',
+  img: 'images/ice-cream.png'
+},
+{
+  name:'milkshake',
+  img: 'images/milkshake.png'
+},
+{
+  name:'pizza',
+  img: 'images.pizza/png'
+},
+{
+  name:'white',
+  img: 'images/white.png'
+}]
